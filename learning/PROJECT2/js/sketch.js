9943c64f5f7f0769a5ac892e3eeabbbf595df0cd
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-03 18:47:40 +0530

    PROJECT2 (In Work!)

---
+++ learning/PROJECT2/js/sketch.js
@@ -0,0 +1,22 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+function preload(){
+
+}
+function setup(){
+    var canvas = createCanvas(800,1200);
+    engine = Engine.create();
+    world = engine.world;
+
+    table = createSprite(1100, 700);    
+}
+
+function draw(){
+    background("green");
+    Engine.update(engine);  
+                                        
+}
