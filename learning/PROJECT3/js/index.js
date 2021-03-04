61a5fff397a32b2c78549583e77866c38c3d9970
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-04 22:33:22 +0530

    Added Announcements, with a way to Enable and Disabling them using a Boolean Value..

--- learning/PROJECT3/js/index.js
+++ learning/PROJECT3/js/index.js
@@ -1,10 +1,18 @@
+let important = false;
 function addHeader(){
-header = document.getElementById('headop');
-console.log("Adding Header");
-let headerStr = `<div class="alert alert-success" role="alert">
-                 <h4 class="alert-heading">Well done!</h4>
-                 <p>You have Successfully added your First Element of Header!</p>
-                 <hr>
-                 <p class="mb-0"> You may also Attach the Book PDF (Under Development)</p>`;
-header.innerHTML += headerStr;
+    header = document.getElementById('headop');
+    if (important == true){
+    console.log("[AVMC] Displaying Announcements!...");
+    let headerStr = `<div class="alert alert-success" role="alert">
+                     <h4 class="alert-heading">Important Note!</h4>
+                     <p>AVMC has Been Shut Down for a Limited Period of Time to due to Mismanagement and 
+                     Shortages of Time of the Admin to Work on it, and due to some more similar reasons, 
+                     check out <a href="https://discord.playavmcgames.ml/">our Discord</a> for more information if required..
+                     </p>
+                     <hr>
+                     <p class="mb-0"> You may also Attach the Book PDF (Under Development)</p>`;
+    header.innerHTML += headerStr;
+    } else if (important == false){
+        console.log("[AVMC] There are No Announcements!")
+    };
 }
