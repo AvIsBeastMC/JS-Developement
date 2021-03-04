8d0b73d25bc94e00352d45779b1faa226990a881
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-04 22:41:24 +0530

    Changed Variables and More to Suit the Purpose of the Project and More!

--- learning/PROJECT3/js/index.js
+++ learning/PROJECT3/js/index.js
@@ -1,17 +1,17 @@
-let important = false;
-function addHeader(){
-    header = document.getElementById('headop');
+let important = true;
+function addAnnouncement(){
+    announcement = document.getElementById('announcement');
     if (important == true){
     console.log("[AVMC] Displaying Announcements!...");
     let headerStr = `<div class="alert alert-success" role="alert">
                      <h4 class="alert-heading">Important Note!</h4>
                      <p>AVMC has Been Shut Down for a Limited Period of Time to due to Mismanagement and 
                      Shortages of Time of the Admin to Work on it, and due to some more similar reasons, 
-                     check out <a href="https://discord.playavmcgames.ml/">our Discord</a> for more information if required..
+                     check out <a href="https://discord.playavmcgames.ml/" target="_blank">our Discord</a> for more information if required..
                      </p>
                      <hr>
                      <p class="mb-0"> You may also Attach the Book PDF (Under Development)</p>`;
-    header.innerHTML += headerStr;
+    announcement.innerHTML += headerStr;
     } else if (important == false){
         console.log("[AVMC] There are No Announcements!")
     };
