8bb16dcd78fe5e48745c2f578c2d09064e90f64c
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-04 22:11:00 +0530

    Added and Tested First Element in the Dynamic Pages!

--- learning/PROJECT3/js/index.js
+++ learning/PROJECT3/js/index.js
@@ -1,1 +1,10 @@
+function addHeader(){
+header = document.getElementById('headop');
 console.log("Adding Header");
+let headerStr = `<div class="alert alert-success" role="alert">
+                 <h4 class="alert-heading">Well done!</h4>
+                 <p>You have Successfully added your First Element of Header!</p>
+                 <hr>
+                 <p class="mb-0"> You may also Attach the Book PDF (Under Development)</p>`;
+header.innerHTML += headerStr;
+}
