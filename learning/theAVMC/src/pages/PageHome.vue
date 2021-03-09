8747f9f04407bf5ade66c9d5b5dfdda95e411105
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-09 19:08:36 +0530

    Added New Search bar, Twitter-like Layout with Suggested News and Responsiveness!

--- learning/theAVMC/src/pages/PageHome.vue
+++ learning/theAVMC/src/pages/PageHome.vue
@@ -2,16 +2,10 @@
   <q-page class="q-pa-lg">
       <q-input bottom-slots v-model="newQweetInput" label="What's poppin'?" counter maxlength="280" :dense="dense">
         <template v-slot:before>
-          <q-avatar>
+          <q-avatar size="xl">
             <img src="https://avmcgames.ml/servermc.png">
           </q-avatar>
         </template>
-
-        <template v-slot:append>
-          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
-          <q-icon name="schedule" />
-        </template>
-
         <template v-slot:hint>
           Post | theAVMC
         </template>
