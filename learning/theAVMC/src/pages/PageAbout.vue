1ade578ccbfccbdd44193c5cced3a4c18b23d35e
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-09 12:20:14 +0530

    Created Template for theAVMC Site with Quasar Installed too!

---
+++ learning/theAVMC/src/pages/PageAbout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+      <h4 class="q-md-none q-mb-md text-weight-bold">About | theAVMC!</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Ullam molestias sed nostrum vel. Perspiciatis fuga optio ab 
+      ullam excepturi! Quidem qui, sed officia sit aliquid 
+      perferendis provident totam. Similique, ad!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageAbout'
+}
+</script>
