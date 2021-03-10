3d99c47fe86a3d0116906168a714012b4ba161b5
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-10 13:23:12 +0530

    Successfully Added Like and Delete Options Workable and Project Almost Finished!

--- learning/theAVMC/src/pages/PageHome.vue
+++ learning/theAVMC/src/pages/PageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="relative-position">
-    <q-scroll-area class="absolute fullscreen">
+    <q-scroll-area class="absolute full-width full-height">
     <div class="q-pa-lg row items-end q-col-gutter-md">
       <div class="col">
         <q-input bottom-slots v-model="newQweetInput" autogrow class="new-post" label="What's poppin'?" counter maxlength="280" :dense="dense">
@@ -21,7 +21,7 @@
     <q-separator class="divider" size="9px" color="grey-3" />
     <q-list separator>
     <transition-group appear enter-active-class="animated fadeIn slow" leave-active-class="animated fadeOut slow">
-      <q-item class="qweet q-py-md" v-for="qweet in qweets" :key="qweet.date">
+      <q-item class="qweet q-py-md" v-for="qweet in qweets" :key="qweet.id">
         <q-item-section avatar top>
           <q-avatar>
             <img src="https://avmcgames.ml/servermc.png">
@@ -37,7 +37,7 @@
         <div class="row justify-between q-mt-sm qweet-icons">
         <q-btn flat size="sm" round color="primary" icon="far fa-comment" />
         <q-btn flat size="sm" round color="primary" icon="fas fa-retweet" />
-        <q-btn flat size="sm" round color="primary" icon="far fa-heart" />
+        <q-btn @click="toggleLiked(qweet)" flat size="sm" :icon="qweet.liked ? 'fas fa-heart' : 'far fa-heart'" round color="primary" />
         <q-btn @click="deleteQweet(qweet)" flat size="sm" round color="primary" icon="fas fa-trash" />
         </div>
         </q-item-section>
@@ -57,14 +57,18 @@ export default {
     return {
       newQweetInput: '',
       qweets: [
-        // {
-        //   content: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?",
-        //   date: 1615353294215
-        // },
-        // {
-        //   content: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
-        //   date: 1615353381558
-        // },
+        //  {
+        //    id: 'ID1',
+        //    content: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?",
+        //    date: 1615353294215,
+        //    liked: false
+        //  },
+        //  {
+        //    id: 'ID2',
+        //    content: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+        //    date: 1615353381558,
+        //    liked: true
+        //  },
       ]
     }
   },
@@ -72,17 +76,36 @@ export default {
     addNewQweet() {
       let newQweet = {
         content: this.newQweetInput,
-        date: Date.now()
+        date: Date.now(),
+        liked: false
       }
-      this.qweets.unshift(newQweet)
-      this.newQweetInput = ''
+      // this.qweets.unshift(newQweet)
+      // Add a new document with a generated id.
+      db.collection("qweets").add(newQweet).then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+      }).catch((error) => {
+          console.error("Error adding document: ", error);
+      });
+            this.newQweetInput = ''
     },
     deleteQweet(qweet) {
-      console.log('Delete theAVMC Post: ', qweet)
-      let dateToDelete = qweet.date
-      let index = this.qweets.findIndex(qweet => qweet.date === dateToDelete)
-      console.log('Index: ', index)
-      this.qweets.splice(index, 1)
+      db.collection("qweets").doc(qweet.id).delete().then(() => {
+      console.log("Document successfully deleted!");
+      }).catch((error) => {
+      console.error("Error removing document: ", error);
+});
+    },
+    toggleLiked(qweet) {
+      db.collection("qweets").doc(qweet.id).update({
+          liked: !qweet.liked
+      })
+      .then(() => {
+          console.log("Document successfully updated!");
+      })
+      .catch((error) => {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+      });
     }
   },
   filters: {
@@ -91,18 +114,23 @@ export default {
     }
   },
   mounted() {
-    db.collection("qweets").onSnapshot(snapshot => {
+    db.collection("qweets").orderBy('date').onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
           let qweetChange = change.doc.data()
+          qweetChange.id = change.doc.id
             if (change.type === "added") {
                 console.log("New Qweet: ", qweetChange);
                 this.qweets.unshift(qweetChange)
             }
             if (change.type === "modified") {
                 console.log("Modified Qweet: ", qweetChange);
+                let index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
+                Object.assign(this.qweets[index], qweetChange)
             }
             if (change.type === "removed") {
                 console.log("Removed Qweet: ", qweetChange);
+                let index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
+                this.qweets.splice(index, 1)
             }
         });
     });
