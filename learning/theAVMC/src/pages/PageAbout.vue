be5a45be662bfc03b3727bb38ba270162c4ef7ac
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-19 09:05:06 +0530

    Changed theQwitter's Icon to the One it should be and Added Link to the Project in the Learning's Page

--- learning/theAVMC/src/pages/PageAbout.vue
+++ learning/theAVMC/src/pages/PageAbout.vue
@@ -2,10 +2,12 @@
   <q-page class="q-pa-lg">
       <h4 class="q-mt-none q-mb-md text-weight-bold">About | theAVMC!</h4>
       <div class="text-body1">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Ullam molestias sed nostrum vel. Perspiciatis fuga optio ab 
-      ullam excepturi! Quidem qui, sed officia sit aliquid 
-      perferendis provident totam. Similique, ad!</p>
+      <p>theAVMC's Qwitter was created by the Web Developer, AvIsBeastMC (Arunnya Varma) who
+        has been primarily working on Web Development (on both Front and Back-End) since almost 2 years
+        and has Expert Experience of HTML, CSS and has Great Experience in JavaScript and its Libraries, NodeJS,
+        NPM and More! Currently Working on Projects that are available on his 
+        <a href="https://github.com/AvIsBeastMC" target="_blank">GitHub Page</a>.
+      </p>
       </div>
   </q-page>
 </template>
