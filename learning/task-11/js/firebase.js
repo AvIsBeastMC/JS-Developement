9951a0dede527221d9b6f945186fcae2f85094e9
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-21 19:41:23 +0530

    Added New Authenticated Login and Signup Project that will Use Firebase Auth (Task-11) [Pt.2]

---
+++ learning/task-11/js/firebase.js
@@ -0,0 +1,17 @@
+console.log("wait")
+var app_firebase = {};
+(function(){
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCoAEx0EdRD-VfONWqc-TEVKqo8ECu0DZw",
+        authDomain: "learning-db1.firebaseapp.com",
+        projectId: "learning-db1",
+        storageBucket: "learning-db1.appspot.com",
+        messagingSenderId: "541085478008",
+        appId: "1:541085478008:web:8809935ba8fe671461518d"
+      };
+    // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      app_firebase = firebase
+    })()
+    
