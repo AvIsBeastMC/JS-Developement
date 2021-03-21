b136b5398ee67bcc675c15218952d671f3935b62
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-21 10:56:09 +0530

    Failed to List Data of the Object converted to Array of Firestore, Array data  Keeps Shuffling! Trying Firebase

--- learning/task-10/index.js
+++ learning/task-10/index.js
@@ -1,13 +1,33 @@
 console.log("ayo");
 var db = firebase.firestore();
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
+// Create a New Document in form1 collection
+
+// db.collection("form1").add({
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+// })
+// .then((docRef) => {
+//     console.log("Document written with ID: ", docRef.id);
+// })
+// .catch((error) => {
+//     console.error("Error adding document: ", error);
+// });
+// Listen for Changes Realtime
+theForm = document.getElementById('tableBody')
+db.collection("form1").onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            let qweetChange = change.doc.data()
+            if (change.type === "added") {
+                console.log("New city: ", change.doc.data());
+                // Plan is to Get the First Value in array[0], and the Second Seperately
+                // than can be stored in a Variable 
+            }
+            if (change.type === "modified") {
+                console.log("Modified city: ", change.doc.data());
+            }
+            if (change.type === "removed") {
+                console.log("Removed city: ", change.doc.data());
+            }
+        });
+    });
