a1c7fffc7c0e00d60aea29d4d43d36795284cb84
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-21 12:16:12 +0530

    Successfully Made the Form So that It Can Insert the Data in Firestore | Working on Reading Data and Displaying it in Table!

--- learning/task-10/index.js
+++ learning/task-10/index.js
@@ -22,8 +22,7 @@ db.collection("form1").onSnapshot((snapshot) => {
                 console.log("New city: ", change.doc.data());
                 var objectReq = change.doc.data()
                 console.log(objectReq.bookName)
-                // Plan is to Get the First Value in array[0], and the Second Seperately
-                // than can be stored in a Variable 
+                // Populate the Table
             }
             if (change.type === "modified") {
                 console.log("Modified city: ", change.doc.data());
@@ -33,3 +32,53 @@ db.collection("form1").onSnapshot((snapshot) => {
             }
         });
     });
+function submitForm() {
+    let formName = document.getElementById('bookName').value
+    let formAuthor = document.getElementById('bookAuthor').value
+    let formType = document.getElementById('bookType').value
+    let submittedOn = Date()
+    // Add a new document with a generated id.
+    db.collection("form1").add({
+        bookName: formName,
+        bookAuthor: formAuthor,
+        bookType: formType,
+        submittedOn: submittedOn,
+    }).then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+    // Display the Submittion to Table
+    console.log('Adding to Table');
+        tableBody = document.getElementById("tableBody");
+        listAllSubm = document.getElementById("listAllSubm")
+        // For Populating the Table!
+        let addForm = `
+                <td>${formName}</td>
+                <td>${formAuthor}</td>
+                <td>${formType}</td>
+                <td>${submittedOn}</td>
+                </tr>`;
+        let listAllSub = `<button type="button" onclick="displayAllSub()" class="btn btn-info">List all Submittions</button><br>`
+        tableBody.innerHTML += addForm;        
+        listAllSubm.innerHTML += listAllSub
+        // For Adding an InnerHTML Alert!
+        bodyAlert = document.getElementById("message");
+        let divopString = `<div class="alert alert-success" role="alert">
+                           <h4 class="alert-heading">Well done!</h4>
+                           <p>You Have Successfully Submitted a Book <b>"${formName}"</b>, <b>${formAuthor}</b> based on <b>${formType}</b>! and you may see it in the Books Table below!</p>
+                           <hr>
+                           <p class="mb-0"> You may also Attach the Book PDF (Under Development)</p>`
+        bodyAlert.innerHTML += divopString;
+        // Removing Form for Spam Protection and One Book Submittion at a Time!
+        formRemove = document.getElementById("formremove");
+        let formRemoveString = `<style>#form{
+            display: none;
+        }</style>`;
+        formRemove.innerHTML += formRemoveString;
+        console.log("Form Display Removed!");
+}
+function displayAllSub() {
+    
+}
