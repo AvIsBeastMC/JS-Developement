169cc253755adae0117620b93100becc8bc40c22
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-28 11:59:01 +0530

    Successfully Created Signed Android APK for theAVMC | Task-11 & Task-8

--- learning/task-11/main/js/script.js
+++ learning/task-11/main/js/script.js
@@ -1,1 +1,11 @@
 console.log("JavaScript 1 Successfully Loaded!")
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
