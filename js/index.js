fb715ab879d78b1be8f49e6c7cba844bdbb9fbcf
AvIsBeastMC <admin@playavmcgames.ml>
2021-03-30 15:49:03 +0530

    Changed Home Index Page including the Design and for SEO too! | 3:49 PM

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-// In this Progress
-// I will learn JS from CodeWithHarry
-        let a;
-        let date;
-        let time;
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        setInterval(() => {
-            a = new Date(); 
-            date = a.toLocaleDateString(undefined, options);
-            time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-            document.getElementById('timeRN').innerHTML = time + "<h4> on " + date;
-        }, 1000);
+// // In this Progress
+// // I will learn JS from CodeWithHarry
+//         let a;
+//         let date;
+//         let time;
+//         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+//         setInterval(() => {
+//             a = new Date(); 
+//             date = a.toLocaleDateString(undefined, options);
+//             time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+//             document.getElementById('timeRN').innerHTML = time + "<h4> on " + date;
+//         }, 1000);
