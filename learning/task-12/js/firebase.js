b23edca0a218aff53c304d3eeb6e82a5a2b675aa
AvIsBeastMC <admin@playavmcgames.ml>
2021-04-01 09:44:52 +0530

    Successfully Created the Basic Structure, Working on Firestore Setup | Task-12

---
+++ learning/task-12/js/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBl5hjn6x17CF85PYRPVdfE7ebQZrMmEMA",
+    authDomain: "newtasks-1.firebaseapp.com",
+    projectId: "newtasks-1",
+    storageBucket: "newtasks-1.appspot.com",
+    messagingSenderId: "84707804050",
+    appId: "1:84707804050:web:8c5afb4f2ef1295edb774b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
